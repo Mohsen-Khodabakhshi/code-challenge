4960c77e156e6a4b50c891b72272d8154967efd0
Mohsen Khodabakhshi <mim.khodabakhshi@gmail.com>
2024-03-19 10:06:56 +0330

    get sensor value by sensor_id

--- backend/core/main.py
+++ backend/core/main.py
@@ -10,6 +10,6 @@ async def startup_event() -> None:
     await events.startup_event_handler(app)
 
 
-@app.get("/")
-def retrieve_sensor_value():
-    return {"value": app.redis.get('sensor')}
+@app.get("/{sensor_id}")
+def retrieve_sensor_value(sensor_id: int):
+    return {"value": app.redis.get(sensor_id)}
